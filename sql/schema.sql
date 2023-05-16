eb89bfe1bd2956e58d92e7828ab04e4d4b4a0882
Felipe Dias <felipedias.dev@gmail.com>
2023-05-15 23:10:32 -0300

    feat: :sparkles: add sqlc and database

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `categories` (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
